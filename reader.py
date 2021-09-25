3b9cc0ab5290d67133380c98f4f72a7d9e3f9260
Zuke ace <zuke0000@gmail.com>
2021-09-24 22:25:54 -0600

    Messing with element tree.

--- reader.py
+++ reader.py
@@ -1,3 +1,20 @@
-if __name__ == "__main__":
-    name = "test"
-    print("hello world")
+import os
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import tostring
+
+file_name = 'small.pwiz.1.1.mzML'
+full_file = os.path.abspath(os.path.join(file_name))
+
+# dom = ET.parse(full_file)
+# tree = ET.parse('small.pwiz.1.1.mzML')
+#iter = tree.iter('chromatogramList')
+#print(iter[0])
+
+
+tree1 = ET.parse('small.pwiz.1.1.mzML')
+root1 = tree1.getroot()
+
+print ('test')
+
+for stuff in root1.findall(".//chromatogramList"):
+    print(stuff)
