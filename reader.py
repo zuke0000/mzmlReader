40c93ef509726ec09b0113b5c0ac7dcf06cd853f
zuke <zuke0000@gmail.com>
2021-10-07 14:42:00 -0600

    extracted binary data and stored it in a dictionary

--- reader.py
+++ reader.py
@@ -2,14 +2,23 @@ import os
 from bs4 import BeautifulSoup
 
 file_name = 'small.pwiz.1.1.mzML'
+dictionaryList = [] # This will hold the dictionaries
+dictionary = {"timeArray": 0, "intensityArray": 0, "zlibCompression": False}
 
 with open('small.pwiz.1.1.mzML', 'r', ) as mzml_file:
   content = mzml_file.read() # Entire file at this point
-
   soup = BeautifulSoup(content, 'lxml') 
-  chromatogram = soup.find_all("chromatogram") # This seems to work for now. Idk why stuff like 'indexedmzML' or 'chromatogramList' dont work
+  chromatogram = soup.find_all("chromatogram")
   
-  for entry in chromatogram:
-    print('test')
+  binaryCounter = 0
+  for chromo in chromatogram:
+      # For now I will assume the first binary data is time and the second is intensity
+      # The way these values are grabbed could be smarter
+    for children in chromo.find_all("binary"):
+      if (binaryCounter == 0):
+        dictionary['timeArray'] = children.text
+      else: 
+        dictionary['intensityArray'] = children.text
   mzml_file.close
+print('dictionary', dictionary)
     
